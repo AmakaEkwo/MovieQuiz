c94d341addf8882e197ed9c9261ea2ae0c7dd158
a.ekwo <a.ekwo@wlc.ac.uk>
2023-04-17 14:36:36 +0100

    I added a question about flowers

--- MovieQuizTemplate/frmQuestion1.vb
+++ MovieQuizTemplate/frmQuestion1.vb
@@ -1,13 +1,15 @@
 ﻿Public Class frmQuestion1
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
-    End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnNext.Click
 
+        If (btnAnswer1.Checked) Then
+            playerScore = playerScore + 1
+        End If
+
+        frmQuestion2.Show()
+        Me.Hide()
     End Sub
 
-    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles btnAnswer1.CheckedChanged
 
-    End Sub
 End Class
